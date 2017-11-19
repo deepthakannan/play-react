85bb01a17996f4a867989c60b829dc88f4329ea8
deepthakannan <deepthakannan@gmail.com>
2017-11-18 20:41:46 -0500

    added compiling arrow function in class properties and callbacks between components

--- app/components/App.js
+++ app/components/App.js
@@ -12,16 +12,41 @@ function Heading(props) {
 function DateTime(props) {
     return (
     <div>
-        <h2>Current time in {props.country} is {props.time.toLocaleTimeString("indian", {timeZone: props.timeZone})}.</h2>
+        <h2>Current time in {props.country} is {props.time.toLocaleTimeString("indian", {timeZone: props.timeZone ? props.timeZone : "America/New_York" })}.</h2>
     </div>
     );        
+}
+
+class TimeZones extends React.Component {
+    constructor(props) {
+        super(props);
+        this.timeZones = ["Asia/Kolkata", "America/New_York", "Asia/Shanghai" ];
+    }
+        
+    timeZoneClicked = (timeZone) => {
+        this.props.timeZoneClicked(timeZone);
+    }
+    
+    render() {
+        var timeZoneButtons = [];
+        this.timeZones.forEach((timeZone) => {
+            timeZoneButtons.push(<button key={timeZone} onClick={ () => this.timeZoneClicked(timeZone) }>{timeZone}</button>);
+        })
+
+        return (
+            <div>
+                {timeZoneButtons}
+            </div>
+        )
+    }    
 }
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             dateTime: new Date(),
-            updateCounter: 0
+            updateCounter: 0,
+            timeZone: ""
         };
     }
 
@@ -44,14 +69,20 @@ class App extends React.Component {
         });
     }
 
+    timeZoneClicked(timeZone) {
+        this.setState((prevState, props) => {
+            return {
+                timeZone: timeZone,
+                updateCounter: 0
+            }
+        });
+    }
+
     render() {
       return <div>
           <Heading text={this.props.name} time={this.state}></Heading>
-          <DateTime time={this.state.dateTime} country="India" timeZone="Asia/Kolkata"></DateTime>
-          <DateTime time={this.state.dateTime} country="Boston-USA" timeZone="America/New_York"></DateTime>
-          {
-              this.state.updateCounter > 10 ? <h3>Updated more than 10 times</h3> : <h3>Updated {this.state.updateCounter} times</h3>
-          }
+          <DateTime time={this.state.dateTime} country={this.state.timeZone} timeZone={this.state.timeZone}></DateTime>
+          <TimeZones timeZoneClicked={(timeZone) => this.timeZoneClicked(timeZone)}></TimeZones>
       </div> 
     }
   }
